5b6408652e4c6f8025de332c293068400ec1c402
Julie Trannois <julie.trannois@smile.fr>
2020-09-24 22:30:03 +0200

    giftlists beginning

---
+++ src/Controller/GiftlistController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Repository\GiftRepository;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class GiftlistController extends AbstractController
+{
+    protected $userRepository;
+
+    protected $giftRepository;
+
+    private $manager;
+
+    public function __construct(EntityManagerInterface $manager, UserRepository $userRepository, GiftRepository $giftRepository)
+    {
+        $this->manager = $manager;
+        $this->userRepository = $userRepository;
+        $this->giftRepository = $giftRepository;
+    }
+
+    /**
+     * @Route("/giftlists", name="giftlists")
+     */
+    public function index()
+    {
+        $users = $this->userRepository->findAll();
+        return $this->render('giftlist/index.html.twig', [
+            'users' => $users,
+        ]);
+    }
+
+    /**
+     * @Route("/giftlist/{firstName}", name="app_giftlists_show", methods={"GET"})
+     */
+    public function show(User $user): Response
+    {
+        return$this->render('giftlist/show.html.twig', compact('user'));
+    }
+
+    /**
+     * @Route("/admin/giftlist/{id<[0-9]+>}/add", name="app_giftlists_add", methods={"GET", "POST"})
+     */
+    public function add(Request $request, User $user): Response
+    {
+        $form = $this->createForm(LibraryType::class, $user, [
+            'method' => 'POST'
+        ]);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->manager->flush();
+
+            $this->addFlash('success',"Le livre a été ajouté à la bibliothèque de $user");
+
+            return $this->redirectToRoute('libraries');
+        }
+
+        return $this->render('libraries/add.html.twig', [
+            'form' => $form->createView(),
+            'user' => $user
+        ]);
+    }
+}
